bc583bc4081cb9bda7628eb1ac063c4f2f81f207
salientsoph <salientsoph@gmail.com>
2022-05-27 23:11:49 +0900

    20220527

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringBootApplication.run(Application.class, args);
+    }
+}
